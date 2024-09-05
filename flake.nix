57be75610c4c7293646a95704c4759a38d5da4dd
Dom Rodriguez <shymega@shymega.org.uk>
2024-09-05 10:12:54 +0100

    fix: Fix `services.ttyd` options
    Essentially:
    
    - `services.ttyd.openFirewall` is no longer a option.
    - `services.ttyd.writeable` is necessary to set.
    
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- flake.nix
+++ flake.nix
@@ -29,7 +29,10 @@
             };
             services = {
               tailscale.enable = true;
-            ttyd.enable = true;
+            ttyd = {
+              enable = true;
+              writeable = true;
+            };
             openssh.enable = true;
             };
             hardware.enableRedistributableFirmware = true;
